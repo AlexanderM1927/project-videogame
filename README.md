bb62ac1d0be0414e8c49439d339077f4f3d3fbde
AlexanderM1927 <jhon71937@gmail.com>
2023-01-12 14:45:15 -0500

    Init project

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a repository for Videogame development.
+
+Tools required:
+
+-phaser
+-vue
+
+In collaboration with @johnpasichana.
+
+Thanks for reading.
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          Fight game
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+
+import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader';
+defineIonPhaser(window);
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup () {
+
+    return {
+    }
+  }
+})
+</script>
