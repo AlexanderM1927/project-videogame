0fbb3a1c38ca72e17b422b6509bacfe20f9d67bb
AlexanderM1927 <jhon71937@gmail.com>
2023-01-23 15:51:00 -0500

    restart btn

--- src/core/scenes/gameover.js
+++ src/core/scenes/gameover.js
@@ -1,11 +1,17 @@
+import { RestartButton } from "../../components/restart-button";
+
 export class GameOver extends Phaser.Scene {
     constructor () {
       super({key: 'gameover'})
+      this.restartButton = new RestartButton(this);
     }
     preload () {
       this.load.image('gameover', 'images/gameover.png')
-    }
+      this.restartButton.preload();
+  }
     create () {
       this.add.image(window.innerWidth / 2, 200, 'gameover')
+
+      this.restartButton.create();
     }
 }
