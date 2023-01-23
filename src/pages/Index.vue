488793ed8dbb96907bbf40c72fa0e95ed53de686
AlexanderM1927 <jhon71937@gmail.com>
2023-01-23 10:25:34 -0500

    Revert "Integration videogame"
    This reverts commit 8df0e9db0b3cfe659e1e2dba59d47a4c0ec831f8.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,12 @@
   <q-page>
     <div class="main-container">
       <div class="game-container">
+        <div @click="initializeGame" v-if="!initialize">
+          <a href="#1"  class="btn btn-start">Initialize</a>
+        </div>
         <ion-phaser
           v-bind:game.prop='game'
+          v-bind:initialize.prop='initialize'
         />
       </div>
     </div>
@@ -15,10 +19,6 @@ import { defineComponent } from 'vue';
 
 import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader';
 import Phaser from 'phaser'
-import { Game } from '../scenes/game.js';
-import { Congratulations } from '../scenes/congratulations.js';
-import { Gameover } from '../scenes/gameover.js';
-import { Menu } from '../scenes/menu.js';
 defineIonPhaser(window);
 
 export default defineComponent({
@@ -30,11 +30,21 @@ export default defineComponent({
         width: "100%",
         height: "100%",
         type: Phaser.AUTO,
-        scene: [Menu, Game, Gameover, Congratulations],
-        physics: {
-          default: 'arcade',
-          arcade: {
-            debug: false
+        scene: {
+          init() {
+            this.cameras.main.setBackgroundColor("#24252A");
+          },
+          create() {
+            this.helloWorld = this.add.text(
+              this.cameras.main.centerX,
+              this.cameras.main.centerY,
+              "Hello World",
+              { font: "1rem Arial",  fill: "#ffffff" }
+            );
+            this.helloWorld.setOrigin(0.5);
+          },
+          update() {
+            this.helloWorld.angle += 1;
           }
         }
       }
